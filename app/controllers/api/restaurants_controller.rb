c298479fb6096641eb0573d91cc2598825bb3065
Jeffrey <chojeffrey@gmail.com>
2019-06-10 16:04:27 -0400

    Fixed demo login. Added React DatePicker

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,7 +1,6 @@
 class Api::RestaurantsController < ApplicationController
     def index
         if params[:keyword]
-            debugger
             @restaurants = Restaurant.search_by_keyword(params[:keyword])
             # .includes(:reviews, :reservations).order(:name)
         end
