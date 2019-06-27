6b98b382eceea768858048eb5156cb2bb09f455f
Jeffrey <chojeffrey@gmail.com>
2019-06-26 20:54:05 -0400

    Fixed users state

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -7,7 +7,7 @@ const usersReducer = (state = {}, action) => {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, state, {[action.currentUser.id]: action.currentUser})
         case RECEIVE_RESTAURANT:
-            return Object.assign({}, action.authors)
+            return Object.assign({}, state, action.authors)
         default:
             return state;
     }
