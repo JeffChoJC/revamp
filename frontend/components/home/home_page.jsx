28c34b86225fc923b84806a73130cae8fefc307b
Jeffrey <chojeffrey@gmail.com>
2019-08-01 10:42:09 -0400

    Fixed search from featured

--- frontend/components/home/home_page.jsx
+++ frontend/components/home/home_page.jsx
@@ -10,27 +10,27 @@ const Home = () => (
             <div id="featured-areas-container">
                 <h2 id="featured-areas-header">Featured Areas</h2>
                 <div className="search-links-container">
-                    <Link to={`/restaurants/search?keyword=new%20york#${ parseDate(new Date()) }`}
+                    <Link to={`/restaurants/search?keyword=new%20york#${ parseDate(new Date()) }#19:00:00`}
                         className="new-york-search">
                         <p className="area-header">New York</p>
                     </Link>
-                    <Link to={`/restaurants/search?keyword=chicago#${parseDate(new Date())}`}
+                    <Link to={`/restaurants/search?keyword=chicago#${parseDate(new Date())}#19:00:00`}
                         className="chicago-search">
                         <p className="area-header">Chicago</p>
                     </Link>
-                    <Link to={`/restaurants/search?keyword=los%20angeles#${parseDate(new Date())}`}
+                    <Link to={`/restaurants/search?keyword=los%20angeles#${parseDate(new Date())}#19:00:00`}
                         className="los-angeles-search">
                         <p className="area-header">Los Angeles</p>
                     </Link>
-                    <Link to={`/restaurants/search?keyword=san%20francisco#${parseDate(new Date())}`}
+                    <Link to={`/restaurants/search?keyword=san%20francisco#${parseDate(new Date())}#19:00:00`}
                         className="san-francisco-search">
                         <p className="area-header">San Francisco</p>
                     </Link>
-                    <Link to={`/restaurants/search?keyword=miami#${parseDate(new Date())}`}
+                    <Link to={`/restaurants/search?keyword=miami#${parseDate(new Date())}#19:00:00`}
                         className="miami-search">
                         <p className="area-header">Miami</p>
                     </Link>
-                    <Link to={`/restaurants/search?keyword=las%20vegas#${parseDate(new Date())}`}
+                    <Link to={`/restaurants/search?keyword=las%20vegas#${parseDate(new Date())}#19:00:00`}
                         className="las-vegas-search">
                         <p className="area-header">Las Vegas</p>
                     </Link>
