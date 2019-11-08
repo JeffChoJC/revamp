1a88d97607f93f801e0ac37c8f180b0f5e7100b3
Jeffrey <chojeffrey@gmail.com>
2019-11-08 13:06:15 -0500

    Update footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="footer-container">
-            <a href="https://github.com/JeffChoJC" className="footer-container-items">My GitHub</a>
-            <a href="https://www.linkedin.com/in/jeffrey-cho-66330574/" className="footer-container-items">My LinkedIn</a>
-            <a href="https://jeffchojc.github.io/portfolio_website/" className="footer-container-items">My Website</a>
+            <a href="https://github.com/JeffChoJC" className="footer-container-items">GitHub</a>
+            <a href="https://www.linkedin.com/in/jeffrey-cho-66330574/" className="footer-container-items">LinkedIn</a>
+            <a href="https://jeffchojc.github.io/portfolio_website/" className="footer-container-items">Website</a>
             <p>Modeled after: &nbsp;</p>
             <a href="https://www.opentable.com/" className="footer-container-items">OpenTable</a>
         </div>
